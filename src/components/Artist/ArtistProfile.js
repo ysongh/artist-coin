9bd816a24354e60ba36969433686af4442149c4e
Louell Sala <louell_sala@yahoo.com>
2019-12-07 18:52:03 -0500

    edited profile

--- src/components/Artist/ArtistProfile.js
+++ src/components/Artist/ArtistProfile.js
@@ -7,7 +7,19 @@ const ArtistProfile = () => {
         <div className="dashboard">
             <ArtistSideDrawer />
             <div className="artistProfile">
-                <h2>Artist Profile</h2>
+                <h2>Songs</h2>
+                <hr></hr>
+                <br></br>
+                <h3>Ready for it</h3>
+                <h3>22</h3>
+                <h3>breathless</h3>
+                <h3>Cold as you</h3>
+                <h3>Cruel Summer</h3>
+                <h3>Crazier</h3>
+                <h3>Breathelss</h3>
+                <h3>I knew you were trouble</h3>
+                <h3>I forgot that you existed</h3>    
+                <h3>You need to calm down</h3>
             </div>
         </div>
     );
