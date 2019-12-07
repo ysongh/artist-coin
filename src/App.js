2141a9cf59d6c249204bb83d4b9463b65670d3ec
You Song Hou <yousonghou@yahoo.com>
2019-12-07 13:31:13 -0500

    set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Artist Coin</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
