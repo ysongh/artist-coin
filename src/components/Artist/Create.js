4b80304fa309fa0f1d23060aef7867ea40e720de
Louell Sala <louell_sala@yahoo.com>
2019-12-08 13:28:30 -0500

    added create

---
+++ src/components/Artist/Create.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Create = () => {
+    return(
+        <div>
+            <center>
+            <br></br> 
+            <br></br>   
+            <br></br>
+            <br></br>
+            <h3>Select Amount:</h3><input></input>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <button>Confirm</button>
+        </div>
+    )
+}
+
+export default Send;
