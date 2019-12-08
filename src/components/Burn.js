3a5a2a469f12022a62c130d0ad84cc2648464d25
Louell Sala <louell_sala@yahoo.com>
2019-12-08 13:13:13 -0500

    sendjs and burnjs

---
+++ src/components/Burn.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Burn = () => {
+    return(
+        <div>
+            <center>
+            <br></br> 
+            <br></br>   
+            <h3> Your address is:</h3><input></input>
+            <br></br>
+            <br></br>
+            <h3>Amount:</h3><input></input>
+            <br></br>
+            <br></br>
+            <h3>Fans Address</h3><input></input>
+            <br></br>
+
+            <button>Burn</button>
+        </div>
+    )
+}
+
+export default Burn;
